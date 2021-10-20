418ede3c8606b2b532cc0cad357a22781549a9fa
hedmon84 <hedmonlopes@gmail.com>
2021-10-20 16:17:30 -0600

    Get  rates data

--- rass-automation/src/main/java/hn/sanservices/rassautomation/Main.java
+++ rass-automation/src/main/java/hn/sanservices/rassautomation/Main.java
@@ -25,8 +25,10 @@ public class Main {
 //            }
 
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
+               // get availability
+//            System.out.println(gson.toJson(getFullLoad("SMB", conn)));
 
-            System.out.println(gson.toJson(getFullLoad("SMB", conn)));
+            System.out.println(gson.toJson(getFullLoad("BBO", "USA", "2022/12/21","2022/12/28",conn)));
 
 
         } catch (SQLException e) {
@@ -67,6 +69,38 @@ public class Main {
     }
 
 
+    static protected List<RatesVO> getFullLoad(String rstCode, String rateStructure, String startDate, String endDate, Connection connection) throws SQLException {
+        List<RatesVO> list = new ArrayList<>();
+
+        try (CallableStatement cs = connection.prepareCall(
+                InternalSQL.GET_RATES)) {
+            cs.setQueryTimeout(60);
+            cs.setString(1, rstCode);
+            cs.setString(2, rateStructure);
+            cs.setString(3, startDate);
+            cs.setString(4, endDate);
+            cs.registerOutParameter(5, OracleTypes.CURSOR);
+            cs.execute();
+
+            try (ResultSet rs = (ResultSet) cs.getObject(5)) {
+                rs.setFetchDirection(ResultSet.FETCH_FORWARD);
+                rs.setFetchSize(7000);
+
+                while (rs.next()) {
+                    String[] values = rs.getString(1).split("\\|");
+                    RatesVO rates = new RatesVO();
+                    hydrateRates(rates, values);
+                    list.add(rates);
+                }
+            }
+        }
+
+        return list;
+    }
+
+
+
+
     static private void hydrateAvailability(AvailabilityVO availability, String[] values) {
 
 
@@ -74,6 +108,31 @@ public class Main {
         availability.setRoomCategory(values[2].substring("room_category=".length()));
         availability.setReservationDate(values[3].substring("reservation_date=".length()));
         availability.setAvailableRooms(values[4].substring("available_rooms=".length()));
+    }
+
+
+
+   static  private void hydrateRates(RatesVO rates, String[] values) {
+
+        rates.setRateStructure(values[1].substring("rate_structure=".length()));
+        rates.setResInsertSource(values[2].substring(("res_insert_source=").length()));
+        rates.setResort(values[3].substring(("resort=").length()));
+        rates.setRoomCategory(values[4].substring(("room_category=").length()));
+        rates.setReserveBeginDate(values[5].substring(("reserve_begin_date=").length()));
+        rates.setStayLength(values[6].substring(("length_of_stay=").length()));
+        rates.setAdultRate(values[7].substring(("adult_rate=").length()));
+        rates.setChildRate(values[8].substring(("child_rate=").length()));
+        rates.setMaxAdult(values[9].substring(("max_adult=").length()));
+        rates.setMaxChild(values[10].substring(("max_children=").length()));
+        rates.setDepartureGateway(values[11].substring(("departure_gateway=").length()));
+        rates.setZone(values[12].substring(("zone=").length()));
+        rates.setState(values[13].substring(("state=").length()));
+        rates.setCountry(values[14].substring(("country=").length()));
+        rates.setSignetOnly(values[15].substring(("signet_only=").length()));
+        rates.setApliedPromo(values[16].substring(("applied_promotions=").length()));
+        rates.setAppliedDiscounts(values[17].substring(("applied_discounts=").length()));
+        rates.setWholesalerOnly(values[18].substring(("wholesaler_only_yn=").length()));
+        rates.setActive(values[19].substring(("active_yn=").length()));
     }
 
 
---
+++ rass-automation/src/main/java/hn/sanservices/rassautomation/RatesVO.java
@@ -0,0 +1,195 @@
+/*
+ * San Services USA, 2021
+ *
+ * All rights reserved.
+ * Copying of this software or parts of this software is a violation
+ * of U.S. and international laws and will be prosecuted.
+ *
+ * Author(s): Hedmon Lopez ( Qa Computer Engineer )
+ *
+ * Created on Wed Oct 20 2:37:14 PM CST 2021
+ *
+ * 10/20/2021 - Hedmon Lopez
+ *      Initial Implementation
+ */
+package hn.sanservices.rassautomation;
+
+/**
+ *
+ * @author Hedmon Lopez
+ */
+public class RatesVO {
+
+    private String rateStructure;
+    private String resInsertSource;
+    private String resort;
+    private String roomCategory;
+    private String reserveBeginDate;
+    private String stayLength;
+    private String adultRate;
+    private String childRate;
+    private String maxAdult;
+    private String maxChild;
+    private String departureGateway;
+    private String zone;
+    private String state;
+    private String country;
+    private String signetOnly;
+    private String apliedPromo;
+    private String appliedDiscounts;
+    private String wholesalerOnly;
+    private String active;
+
+    public String getRateStructure() {
+        return rateStructure;
+    }
+
+    public void setRateStructure(String rateStructure) {
+        this.rateStructure = rateStructure;
+    }
+
+    public String getResInsertSource() {
+        return resInsertSource;
+    }
+
+    public void setResInsertSource(String resInsertSource) {
+        this.resInsertSource = resInsertSource;
+    }
+
+    public String getResort() {
+        return resort;
+    }
+
+    public void setResort(String resort) {
+        this.resort = resort;
+    }
+
+    public String getRoomCategory() {
+        return roomCategory;
+    }
+
+    public void setRoomCategory(String roomCategory) {
+        this.roomCategory = roomCategory;
+    }
+
+    public String getReserveBeginDate() {
+        return reserveBeginDate;
+    }
+
+    public void setReserveBeginDate(String reserveBeginDate) {
+        this.reserveBeginDate = reserveBeginDate;
+    }
+
+    public String getStayLength() {
+        return stayLength;
+    }
+
+    public void setStayLength(String stayLength) {
+        this.stayLength = stayLength;
+    }
+
+    public String getAdultRate() {
+        return adultRate;
+    }
+
+    public void setAdultRate(String adultRate) {
+        this.adultRate = adultRate;
+    }
+
+    public String getChildRate() {
+        return childRate;
+    }
+
+    public void setChildRate(String childRate) {
+        this.childRate = childRate;
+    }
+
+    public String getMaxAdult() {
+        return maxAdult;
+    }
+
+    public void setMaxAdult(String maxAdult) {
+        this.maxAdult = maxAdult;
+    }
+
+    public String getMaxChild() {
+        return maxChild;
+    }
+
+    public void setMaxChild(String maxChild) {
+        this.maxChild = maxChild;
+    }
+
+    public String getDepartureGateway() {
+        return departureGateway;
+    }
+
+    public void setDepartureGateway(String departureGateway) {
+        this.departureGateway = departureGateway;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getSignetOnly() {
+        return signetOnly;
+    }
+
+    public void setSignetOnly(String signetOnly) {
+        this.signetOnly = signetOnly;
+    }
+
+    public String getApliedPromo() {
+        return apliedPromo;
+    }
+
+    public void setApliedPromo(String apliedPromo) {
+        this.apliedPromo = apliedPromo;
+    }
+
+    public String getAppliedDiscounts() {
+        return appliedDiscounts;
+    }
+
+    public void setAppliedDiscounts(String appliedDiscounts) {
+        this.appliedDiscounts = appliedDiscounts;
+    }
+
+    public String getWholesalerOnly() {
+        return wholesalerOnly;
+    }
+
+    public void setWholesalerOnly(String wholesalerOnly) {
+        this.wholesalerOnly = wholesalerOnly;
+    }
+
+    public String getActive() {
+        return active;
+    }
+
+    public void setActive(String active) {
+        this.active = active;
+    }
+
+}
